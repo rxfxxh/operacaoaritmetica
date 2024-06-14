16a417f7eb19ae4de13c82d0ed65be5f0ff6a2a3
Rafael <160151456+rxfxxh@users.noreply.github.com>
2024-06-14 12:17:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+var x = 10;
+var y = 5;
+
+
+var soma = x + y;
+var sub = x - y;
+var mult = x * y;
+var div = x / y;
+var mod = x % y;
+
+console.log(soma);
+console.log(sub);
+// console.log(mult);
+console.log(x / y);
+console.log(x % y);
+
+z = x++;
+w = y++;
+
+console.log(z);
+console.log(w);
+
